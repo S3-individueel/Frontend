500ade8a2abadcf47cbe669d2673ef6ff2da3a9a
MikevEngelen <47856076+MikevEngelen@users.noreply.github.com>
2023-06-05 20:08:02 +0200

    Reverted BaseURL for dev purposes

--- volksmond/src/http-common.tsx
+++ volksmond/src/http-common.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://volksmondapi-app-20230601005726.delightfulpond-1e26431f.northeurope.azurecontainerapps.io/api",
+    //baseURL: "https://volksmondapi-app-20230601005726.delightfulpond-1e26431f.northeurope.azurecontainerapps.io/api",
+    baseURL: "https://localhost:44379/api",
     headers: {
         "Content-type": "application/json"
     }
