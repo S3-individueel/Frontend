a861f2b7666d118800915d36fed4eba0c9cb5729
MikevEngelen <47856076+MikevEngelen@users.noreply.github.com>
2023-06-06 15:46:00 +0200

    Fixed citizen type

--- volksmond/src/types/citizen.tsx
+++ volksmond/src/types/citizen.tsx
@@ -2,7 +2,7 @@ export default interface ICitizenData {
     id?: any | null,
     firstname: string,
     lastname: string,
-    dateOfBirth: Date,
+    dateOfBirth?: Date,
     gender: string,
     photo: string
 }
