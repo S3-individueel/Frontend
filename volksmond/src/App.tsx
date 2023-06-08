9c2f799dd24a88032746ca384e39f2f522a146c4
MikevEngelen <47856076+MikevEngelen@users.noreply.github.com>
2023-06-08 16:34:37 +0200

    Styled header

--- volksmond/src/App.tsx
+++ volksmond/src/App.tsx
@@ -1,7 +1,11 @@
 import { BrowserRouter, Route, Routes, NavLink, Outlet } from 'react-router-dom';
 import React, { useState, useEffect } from "react";
-import logo from './logo.svg';
+import userIcon from './assets/icons/user.png';
+import searchIcon from './assets/icons/magnifying-glass.png';
+import './styles/reset.scss';
 import './styles/04_template/t-header.scss';
+import './styles/01_atom/a-searchBar.scss';
+import './styles/main.scss';
 
 function App() {
   return (
@@ -9,31 +13,15 @@ function App() {
       <header className="t-header">
         <div>
           <a href='/'>
-            <img src={logo} className="App-logo" alt="logo" />
+            <img src={userIcon} className="App-logo" alt="logo" />
           </a>
 
-          <ul>
-            <li>
-              <NavLink to="/groups">
-                Groups
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/events">
-                Events
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/profile">
-                Profile
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/movies">
-                Movies
-              </NavLink>
-            </li>
-          </ul>
+          <div className='a-searchBar'>
+            <form>
+              <input type='text' placeholder='Search for a topic...'/>
+              <button type='submit'><img src={searchIcon} /></button>
+            </form>
+          </div>
         </div>
       </header>
 
