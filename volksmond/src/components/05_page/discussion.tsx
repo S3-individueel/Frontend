44af716734ffb242555280db1b0eb48eda1dc873
MikevEngelen <47856076+MikevEngelen@users.noreply.github.com>
2023-06-11 00:33:03 +0200

    Fixed solution posting

--- volksmond/src/components/05_page/discussion.tsx
+++ volksmond/src/components/05_page/discussion.tsx
@@ -5,12 +5,15 @@ import '../../styles/03_organism/o-solutionsList.scss'
 import '../../styles/02_molecule/m-commentsList.scss'
 import { useParams } from "react-router-dom";
 import SolutionsList from "../03_organism/o-solutionsList";
+import SolutionDataService from "../../services/solution.service";
 
 type Props = {};
 
 const DiscussionPage: React.FC<Props> = () => {
     const [problems, setProblems] = useState<IProblemData>();
     const { discussionId, solutionId } = useParams<{ discussionId: string, solutionId: string }>();
+    const [solutionTitle, setSolutionTitle] = useState("");
+    const [solutionText, setSolutionText] = useState("");
 
     useEffect(() => {
         retrieveAllCitizens();
@@ -27,7 +30,7 @@ const DiscussionPage: React.FC<Props> = () => {
             });
     };
 
-    const postCitizen = (e: React.FormEvent): void => {
+    const postProblem = (e: React.FormEvent): void => {
         e.preventDefault();
 
         const target = e.target as typeof e.target & {
@@ -36,8 +39,6 @@ const DiscussionPage: React.FC<Props> = () => {
             title: { value: string },
             description: { value: string },
             postDate?: { value: Date },
-            gender: { value: string },
-            photo: { value: string }
         };
 
         const problem: IProblemData = {
@@ -58,6 +59,40 @@ const DiscussionPage: React.FC<Props> = () => {
             });
     };
 
+    const createSolution = (e: React.FormEvent): void => {
+        e.preventDefault();
+
+        const solutionData = {
+            title: solutionTitle,
+            text: solutionText,
+            citizenId: 1, // Replace with the actual citizenId value
+            problemId: discussionId,
+        };
+
+        SolutionDataService.create(solutionData)
+            .then((response: any) => {
+                // Handle the successful creation of the solution
+                console.log(response.data);
+                // You can update the state or perform any other necessary actions
+            })
+            .catch((error: Error) => {
+                // Handle the error if necessary
+                console.log(error);
+            });
+
+        // Reset the form fields
+        setSolutionTitle("");
+        setSolutionText("");
+    };
+
+    const handleSolutionTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSolutionTitle(e.target.value);
+    };
+
+    const handleSolutionTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setSolutionText(e.target.value);
+    };
+
     const deleteCitizen = (e: React.FormEvent): void => {
         e.preventDefault();
         const target = e.target as typeof e.target & {
@@ -75,9 +110,31 @@ const DiscussionPage: React.FC<Props> = () => {
             </p>
             <strong>Referendum starts {problems?.postDate?.toString()}</strong>
 
+            <h2>Create solution</h2>
+            <form onSubmit={createSolution}>
+                <div>
+                    <label htmlFor="solutionTitle">Title:</label>
+                    <input
+                        type="text"
+                        id="solutionTitle"
+                        value={solutionTitle}
+                        onChange={handleSolutionTitleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="solutionText">Text:</label>
+                    <textarea
+                        id="solutionText"
+                        value={solutionText}
+                        onChange={handleSolutionTextChange}
+                    ></textarea>
+                </div>
+                <button type="submit">Create</button>
+            </form>
+
             <h2>Suggested solutions</h2>
 
-            <SolutionsList discussionId={discussionId}/>
+            <SolutionsList discussionId={discussionId} />
         </div>
     );
 };
